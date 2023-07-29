9201131239249ed8f274b2677f651644b92fb483
mynuni <elelaka0000@gmail.com>
2023-07-29 19:15:34 +0900

    chore: Initial project setup (#1)

---
+++ src/main/java/com/my/worldwave/WorldWaveApplication.java
@@ -0,0 +1,13 @@
+package com.my.worldwave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldWaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldWaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/worldwave/WorldWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.worldwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
