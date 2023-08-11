66530d647c2485145ca273952fa2f491e7290ab0
mynuni <elelaka0000@gmail.com>
2023-08-11 20:15:44 +0900

    feat: Add exception handler to return object validation results in a list

--- src/main/java/com/my/worldwave/exception/ExceptionControllerAdvice.java
+++ src/main/java/com/my/worldwave/exception/ExceptionControllerAdvice.java
@@ -3,8 +3,14 @@ package com.my.worldwave.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -12,7 +18,18 @@ public class ExceptionControllerAdvice {
 
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ExceptionResponse> handleBadRequestException(BadRequestException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionResponse(e.getMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionResponse(e.getMessage(), e.getClass().getSimpleName()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ExceptionResponse>> handleValidationException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        List<ExceptionResponse> responses = fieldErrors.stream()
+                .map(fieldError -> new ExceptionResponse(e.getClass().getSimpleName(), fieldError.getDefaultMessage()))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responses);
     }
 
 }
