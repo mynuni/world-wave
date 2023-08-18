8e64092dc0c0d9623e458e42c0b506b800154c82
mynuni <elelaka0000@gmail.com>
2023-08-18 22:00:58 +0900

    fix: Override equals method for news caching (#13)

--- src/main/java/com/my/worldwave/api/news/dto/NewsRequestDto.java
+++ src/main/java/com/my/worldwave/api/news/dto/NewsRequestDto.java
@@ -1,14 +1,33 @@
 package com.my.worldwave.api.news.dto;
 
-import com.my.worldwave.api.news.model.News;
 import lombok.Getter;
 import lombok.Setter;
-import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @Getter
 @Setter
 public class NewsRequestDto {
     private String country;
     private int pageSize;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        NewsRequestDto newsRequestDto = (NewsRequestDto) obj;
+        return Objects.equals(this.country, newsRequestDto.country) && this.pageSize == newsRequestDto.pageSize;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(country, pageSize);
+    }
 
 }
