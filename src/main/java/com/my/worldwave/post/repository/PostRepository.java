cd7fa7b7a872fc29a5f7052f714252a2a94dec89
mynuni <elelaka0000@gmail.com>
2023-08-27 15:45:05 +0900

    refactor: 검색, 정렬 쿼리 통합 및 분리

--- src/main/java/com/my/worldwave/post/repository/PostRepository.java
+++ src/main/java/com/my/worldwave/post/repository/PostRepository.java
@@ -8,20 +8,67 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    @Query(value = "SELECT p FROM Post p LEFT JOIN FETCH p.comments",
+
+    @Query(value = "SELECT p FROM Post p " +
+                   "LEFT JOIN FETCH p.comments",
             countQuery = "SELECT COUNT(p) FROM Post p")
     Page<Post> findAllPosts(Pageable pageable);
 
-    @Query(value = "SELECT p FROM Post p LEFT JOIN FETCH p.comments WHERE p.country = :country",
-            countQuery = "SELECT COUNT(p) FROM Post p WHERE p.country = :country")
-    Page<Post> findAllPostsByCountry(@Param("country") String country, Pageable pageable);
+    // 검색과 정렬 조건 동시 적용
+    @Query(value = "SELECT p FROM Post p " +
+            "LEFT JOIN FETCH p.comments " +
+            "WHERE p.country = :country " +
+            "AND (:content IS NULL OR p.content LIKE %:content%) " +
+            "ORDER BY CASE WHEN :sortCond = 'likes' THEN p.likesCount END DESC, " +
+                     "CASE WHEN :sortCond = 'comments' THEN p.commentsCount END DESC, " +
+                     "p.createdAt DESC",
+            countQuery = "SELECT COUNT(p) FROM Post p " +
+                    "WHERE p.country = :country " +
+                    "AND (:content IS NULL OR p.content LIKE %:content%)")
+    Page<Post> findAllContentContainingWithSorting(@Param("country") String country,
+                                                   @Param("content") String searchKeyword,
+                                                   @Param("sortCond") String sortCond, Pageable pageable);
 
-    @Query(value = "SELECT p FROM Post p LEFT JOIN FETCH p.comments WHERE p.country = :country",
-            countQuery = "SELECT COUNT(p) FROM Post p WHERE p.country = :country GROUP BY p.id ORDER BY p.likesCount DESC, p.createdAt desc")
-    Page<Post> findAllPostsByCountryOrderByLikes(@Param("country") String country, Pageable pageable);
+    // 검색만 분리
+    @Query(value = "SELECT p FROM Post p " +
+            "LEFT JOIN FETCH p.comments " +
+            "WHERE p.country = :country " +
+            "AND (:content IS NULL OR p.content LIKE %:content%) " +
+            "ORDER BY p.createdAt DESC",
+            countQuery = "SELECT COUNT(p) FROM Post p " +
+                    "WHERE p.country = :country " +
+                    "AND (:content IS NULL OR p.content LIKE %:content%)")
+    Page<Post> findAllContentContaining(@Param("country") String country, @Param("content") String searchKeyword, Pageable pageable);
 
-    @Query(value = "SELECT p FROM Post p LEFT JOIN FETCH p.comments WHERE p.country = :country",
-            countQuery = "SELECT COUNT(p) FROM Post p WHERE p.country = :country GROUP BY p.id ORDER BY p.commentsCount DESC, p.createdAt desc")
-    Page<Post> findAllPostsByCountryOrderByComments(@Param("country") String country, Pageable pageable);
+    // 정렬만 분리
+    @Query(value = "SELECT p FROM Post p " +
+            "LEFT JOIN FETCH p.comments " +
+            "WHERE p.country = :country " +
+            "ORDER BY CASE WHEN :sortCond = 'likes' THEN p.likesCount END DESC, " +
+            "CASE WHEN :sortCond = 'comments' THEN p.commentsCount END DESC, " +
+            "p.createdAt DESC",
+            countQuery = "SELECT COUNT(p) FROM Post p " +
+                    "WHERE p.country = :country ")
+    Page<Post> findAllWithSort(@Param("country") String country, @Param("sortCond") String sortCond, Pageable pageable);
+
+    // 좋아요순 정렬 조건 분리
+    @Query(value = "SELECT p FROM Post p " +
+                   "LEFT JOIN FETCH p.comments " +
+                   "WHERE p.country = :country " +
+                   "ORDER BY p.likesCount DESC, p.createdAt desc",
+            countQuery = "SELECT COUNT(p) FROM Post p " +
+                         "WHERE p.country = :country " +
+                         "GROUP BY p.id")
+    Page<Post> findAllOrderByLikes(@Param("country") String country, Pageable pageable);
+
+    // 댓글수 정렬 조건 분리
+    @Query(value = "SELECT p FROM Post p " +
+                   "LEFT JOIN FETCH p.comments " +
+                   "WHERE p.country = :country " +
+                   "ORDER BY p.commentsCount DESC, p.createdAt desc",
+            countQuery = "SELECT COUNT(p) FROM Post p " +
+                         "WHERE p.country = :country " +
+                         "GROUP BY p.id ")
+    Page<Post> findAllOrderByComments(@Param("country") String country, Pageable pageable);
 
 }
