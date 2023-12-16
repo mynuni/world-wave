345e26c9b342047b3df6c5585b815ca13a1c27a5
mynuni <elelaka0000@gmail.com>
2023-12-16 23:13:57 +0900

    refactor: 날짜 auditing 엔티티 물리 이름 설정

--- src/main/java/com/my/worldwave/util/domain/BaseEntity.java
+++ src/main/java/com/my/worldwave/util/domain/BaseEntity.java
@@ -16,9 +16,10 @@ import java.time.LocalDateTime;
 public abstract class BaseEntity {
 
     @CreatedDate
-    @Column(updatable = false)
+    @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Column(name = "last_updated_at")
     private LocalDateTime lastUpdatedAt;
 }
