73d13b5fdf5a4c2305c0e3709dfaa9c015263f25
mynuni <elelaka0000@gmail.com>
2023-12-25 23:01:11 +0900

    refactor: 참가자마다 최초 입장 시간이 저장되도록 변경

--- src/main/java/com/my/worldwave/chat/entity/ChatRoom.java
+++ src/main/java/com/my/worldwave/chat/entity/ChatRoom.java
@@ -1,15 +1,16 @@
 package com.my.worldwave.chat.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -20,6 +21,18 @@ public class ChatRoom {
     private String chatRoomId;
     private String chatRoomName;
     private Long creatorId;
-    private List<String> participantIds;
+    private List<Participant> participants = new ArrayList<>();
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public static class Participant {
+        private String memberId;
+
+        // 회원의 최초 입장 시간 ~ 현재 시간까지의 채팅 내역 조회를 위해 필요
+        private LocalDateTime enteredAt;
+    }
 
 }
